6070e213653053392c38d7160978897a95a9c8db
Mike <mcmiller905@gmail.com>
2022-12-01 10:11:18 -0500

    day 1 done

--- DayCode/day1.py
+++ DayCode/day1.py
@@ -1,8 +1,38 @@
 def runPart1():
     inputFile = open('InputFiles/day1Input.txt', 'r')
     numbers = inputFile.readlines()
-
-    print(numbers)
+    elfTotal = 0
+    biggestElf = 0
+    index = 0
+    while(True):
+        if(index >= len(numbers)):
+            break
+        currentLine = numbers[index]
+        if(currentLine == "\n"):
+            if(elfTotal > biggestElf):
+                biggestElf = elfTotal
+            elfTotal = 0
+        else:
+            elfTotal = elfTotal + int(currentLine)
+        index+=1
+    print(biggestElf)
 
 def runPart2():
-    print("part2")
+    inputFile = open('InputFiles/day1Input.txt', 'r')
+    numbers = inputFile.readlines()
+    elfTotal = 0
+    elfArray = []
+    index = 0
+    while(True):
+        if(index >= len(numbers)):
+            break
+        currentLine = numbers[index]
+        if(currentLine == "\n"):
+            elfArray.append(elfTotal)
+            elfTotal = 0
+        else:
+            elfTotal = elfTotal + int(currentLine)
+        index+=1
+    elfArray.sort(reverse=True)
+    top3Total = elfArray[0] + elfArray[1] + elfArray[2]
+    print(top3Total)
--- main.py
+++ main.py
@@ -1,16 +1,32 @@
 # Main runner of all the Advent of Code 2021 code. Change the 'day' variable to
-# run the different day's code
+# run the different day's code, or set to 0 to run all
 from importlib import import_module
 
 day = 1
+runPart1 = True
+runPart2 = True
 
-print()
-print("*--------------------")
-print("*  DAY " + str(day))
-print("*--------------------")
-print()
-module = import_module('DayCode.day'+str(day))
-part1 = getattr(module, 'runPart1')
-part1()
-# part2 = getattr(module, 'runPart2')
-# part2()
+def runDay(dayNum):
+    print()
+    print("*--------------------")
+    print("*  DAY " + str(dayNum))
+    print("*--------------------")
+    print()
+    module = import_module('DayCode.day'+str(dayNum))
+    if(runPart1):
+        part1 = getattr(module, 'runPart1')
+        print("Part 1:")
+        part1()
+    if(runPart2):
+        part2 = getattr(module, 'runPart2')
+        print("Part 2:")
+        part2()
+
+if(day == 0):
+    i = 1
+    while(i < 26):
+        runDay(i)
+        i+=1
+else:
+    runDay(day)
+
